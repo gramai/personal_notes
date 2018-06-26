c087d35a934ca9a8930cf67efb00ef7ef683daec
Grama Ioan-Emanuel <gramai.emanuel@gmail.com>
2018-06-26 16:45:15 +0300

    Update refined_strings.sql

--- Learning_SQL/src/refined_strings.sql
+++ Learning_SQL/src/refined_strings.sql
@@ -1,3 +1,17 @@
+/*
+Simplified Library Database
+CREATE TABLE books 
+	(
+		book_id INT NOT NULL AUTO_INCREMENT,
+		title VARCHAR(100),
+		author_fname VARCHAR(100),
+		author_lname VARCHAR(100),
+		released_year INT,
+		stock_quantity INT,
+		pages INT,
+		PRIMARY KEY(book_id)
+	);
+*/
 -----------------------------------------------------------------------
 /*
 1.Select All Story Collections 
