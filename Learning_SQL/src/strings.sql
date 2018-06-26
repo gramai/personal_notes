d0da2d6bc4337916bb69c6e53062f98565377129
Grama Ioan-Emanuel <gramai.emanuel@gmail.com>
2018-06-26 16:44:08 +0300

    Update strings.sql

--- Learning_SQL/src/strings.sql
+++ Learning_SQL/src/strings.sql
@@ -1,4 +1,18 @@
+/*
+Simplified Library
+CREATE TABLE books 
+	(
+		book_id INT NOT NULL AUTO_INCREMENT,
+		title VARCHAR(100),
+		author_fname VARCHAR(100),
+		author_lname VARCHAR(100),
+		released_year INT,
+		stock_quantity INT,
+		pages INT,
+		PRIMARY KEY(book_id)
+	);
 
+*/
 -----------------------------------------------------------------------
 
 /*
